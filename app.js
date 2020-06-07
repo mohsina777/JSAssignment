e509f6c4431787dc325c8db10eb49b5cc54244b4
mohsin ali <mohsinnaqvi1003@gmail.com>
2020-06-07 13:56:34 +0500

    JS

---
+++ app.js
@@ -0,0 +1,72 @@
+﻿//Alerts
+    alert('Javascript Alert \n \n \n Error! Please enter a valid password.');
+    alert('Javascript Alert \n \n \n Welcome to JS Land...\n Happy Codding !');
+    alert('Javascript Alert \n \n \n Welcome to JS Land..');
+    alert('Javascript Alert \n \n \n Happy Codding !');
+    alert("Hello ! I can run JS through my web browser's console");
+    console.log(alert);
+
+
+
+//Variables for Strings
+
+    var username;
+    var myname = "Syed Mohsin Ali";
+    username = myname;
+
+
+    var message = "Hello World !";
+     alert(message);
+
+     var name = "John Doe";
+     var age = "15 year old";
+     var course = "Certified Mobile Application Development";
+
+     alert(name);
+     alert(age);
+     alert(course);
+
+     var pizza = "PIZZA";
+     alert(pizza + "\n"  + pizza[0] + pizza[1] + pizza[2] + pizza[3] + "\n" + pizza[0] + pizza[1] + pizza[2] + "\n" + pizza[0] + pizza[1] + "\n" + pizza[0]);
+
+
+     var email = "mohsinnaqvi1003@gmail.com";
+     alert("My email address is" + email);
+
+     var book = " A smarter way to learn JavaScript";
+     alert("I am trying to learn from the Book" + book);
+
+
+     document.write("Yeah ! I can write HTML Content through JavaScript" + "<br><br>");
+
+     var str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+     alert(str);
+
+
+
+//VARIABLES FOR NUMBERS
+
+
+     var age = 15;
+     alert("I am " + age + " year old");
+
+//var count = document.l
+
+     var birthYear = 1994;
+     document.write("My birth years is " + birthYear + "<br>");
+
+     document.write("Data type of my decalred variable is " + typeof birthYear + "<br><br>");
+
+     var VisitorName, ProName, Qty;
+
+     VisitorName = "John Doe";
+     ProName = "T-Shirt(s)";
+     Qty = 5;
+
+     document.writeln("<b>" + VisitorName + "</b>" + " ordered " + "<b>" + Qty + " " + ProName + "</b>" + " on XYZ Clothing Store" + "<br><br>");
+
+
+
+//VARIABLE NAMES: LEGAL & ILLEGAL
+
+var va
